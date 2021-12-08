8180008e33fe9fa49ff99c64d8d645cd3c69757d
linbossgebest <809621397@qq.com>
2021-12-08 18:13:12 +0800

    update

---
+++ LeetCode_HOT_100/22Q.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_HOT_100
+{
+    //    面试题 02.06. 回文链表
+    //编写一个函数，检查输入的链表是否是回文的。
+
+
+
+    //示例 1：
+
+    //输入： 1->2
+    //输出： false 
+    //示例 2：
+
+    //输入： 1->2->2->1
+    //输出： true 
+
+
+    //进阶：
+    //你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+    public class Solution22
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+        public bool IsPalindrome(ListNode head)
+        {
+
+        }
+    }
+}
