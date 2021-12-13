06f4b586df7fd6659e244b33dd5a90506498cb1f
linbossgebest <809621397@qq.com>
2021-12-13 17:47:07 +0800

    update DFS&BFS

--- LeetCode_HOT_100/31Q.cs
+++ LeetCode_HOT_100/31Q.cs
@@ -24,7 +24,29 @@ namespace LeetCode_HOT_100
     //图中可能存在自环和平行边。
     public class Solution31
     {
-        public bool FindWhetherExistsPath(int n, int[][] graph, int start, int target)
+        public bool Test()
+        {
+            int[][] graph = new int[][]
+            {
+                 new int[]{ 0,1},
+                 new int[]{ 0,2},
+                 new int[]{ 0,4},
+                 new int[]{ 0,4},
+                 new int[]{ 0,1},   new int[]{1, 3},   new int[]{1, 4},   new int[]{1, 3},   new int[]{2, 3},   new int[]{3, 4}
+            };
+            //return FindWhetherExistsPath1(5, graph, 0, 4);
+            return FindWhetherExistsPath2(5, graph, 0, 4);
+        }
+
+        /// <summary>
+        /// BFS
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="graph"></param>
+        /// <param name="start"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public bool FindWhetherExistsPath1(int n, int[][] graph, int start, int target)
         {
             List<List<int>> list = new List<List<int>>();
             for (int i = 0; i < n; i++)
@@ -45,17 +67,47 @@ namespace LeetCode_HOT_100
                 for (int j = 0; j < list[cur].Count; j++)
                 {
                     int node = list[cur][j];
-                    if (!visited[node]) 
+                    if (!visited[node])
                     {
                         visited[node] = true;
                         if (node == target)
                             return true;
 
-                        queue.Enqueue(node);                   
+                        queue.Enqueue(node);
                     }
                 }
             }
+            return false;
+        }
 
+        public bool FindWhetherExistsPath2(int n, int[][] graph, int start, int target)
+        {
+            Dictionary<int, HashSet<int>> dic = new Dictionary<int, HashSet<int>>();
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if (!dic.ContainsKey(graph[i][0]))
+                {
+                    dic.Add(graph[i][0], new HashSet<int>());
+                }
+                dic[graph[i][0]].Add(graph[i][1]);
+            }
+
+            return DFS(start, target, dic, new HashSet<int>());
+        }
+
+        private bool DFS(int start, int target, Dictionary<int, HashSet<int>> dic, HashSet<int> visited)
+        {
+            if (visited.Contains(start) || !dic.ContainsKey(start))
+                return false;
+            else if (dic[start].Contains(target))
+                return true;
+
+            visited.Add(start);
+            foreach (var item in dic[start])
+            {
+                if (DFS(item, target, dic, visited))
+                    return true;
+            }
             return false;
         }
     }
--- LeetCode_HOT_100/Program.cs
+++ LeetCode_HOT_100/Program.cs
@@ -7,20 +7,23 @@ namespace LeetCode_HOT_100
         static void Main(string[] args)
         {
             //string str = "cbbd";
-            string str = "babad";
-            Solution5 s5 = new Solution5();
-            var temp1=s5.LongestPalindrome(str);
-            Console.WriteLine(temp1);
+            //string str = "babad";
+            //Solution5 s5 = new Solution5();
+            //var temp1=s5.LongestPalindrome(str);
+            //Console.WriteLine(temp1);
 
 
 
-            var matrix = new int[][] {
-            new int[]{ 1, 2, 3 },
-            new int[]{ 4,5,6},
-            new int[] { 7,8,9}
-            };
-            Solution14 s14 = new Solution14();
-            s14.Rotate(matrix);
+            //var matrix = new int[][] {
+            //new int[]{ 1, 2, 3 },
+            //new int[]{ 4,5,6},
+            //new int[] { 7,8,9}
+            //};
+            //Solution14 s14 = new Solution14();
+            //s14.Rotate(matrix);
+
+            Solution31 s31 = new Solution31();
+            s31.Test();
 
 
             Console.WriteLine("Hello World!");
