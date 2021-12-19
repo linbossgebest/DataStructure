bffe00810839c58b01675019495360e1347e9c1b
linbossgebest <809621397@qq.com>
2021-12-19 22:15:05 +0800

    add q

---
+++ LeetCode_HOT_100/54Q.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_HOT_100
+{
+    //    104. 二叉树的最大深度
+    //给定一个二叉树，找出其最大深度。
+
+    //二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+    //说明: 叶子节点是指没有子节点的节点。
+
+    //示例：
+    //给定二叉树[3, 9, 20, null, null, 15, 7]，
+
+    //    3
+    //   / \
+    //  9  20
+    //    /  \
+    //   15   7
+    //返回它的最大深度 3 。
+    public class Solution54
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+
+        public int MaxDepth1(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            int depth = 0;
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                while (size > 0)
+                {
+                    var node = queue.Dequeue();
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+
+                    size--;
+                }
+                depth++;
+            }
+            return depth;
+        }
+    }
+}
---
+++ LeetCode_HOT_100/55Q.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_HOT_100
+{
+    //    226. 翻转二叉树
+    //翻转一棵二叉树。
+
+    //示例：
+
+    //输入：
+
+    //     4
+    //   /   \
+    //  2     7
+    // / \   / \
+    //1   3 6   9
+    //输出：
+
+    //     4
+    //   /   \
+    //  7     2
+    // / \   / \
+    //9   6 3   1
+    //备注:
+    //这个问题是受到 Max Howell 的 原问题 启发的 ：
+
+    //谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+    public class Solution55
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            var temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            return root;
+        }
+    }
+}
