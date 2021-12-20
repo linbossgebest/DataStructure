efe05dd035e1bbf2425560359a0f37129827de9b
linbossgebest <809621397@qq.com>
2021-12-20 16:34:02 +0800

    add gc

---
+++ GCHandler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace GCHandler
+{
+
+    //我们在使用c#托管代码时，内存地址和GC回收那不是我们关心的，CLR已经给我们暗箱操作。
+    //但是如果我们在c#中调用了一个非托管代码，比如vc的DLL,而且他有个回调函数，需要引用c#中的某个对象并操作，
+    //这时候你就得要小心了。
+    //要是非托管代码中用到得托管代码那个对象被GC给回收了，这时候就会报内存错误。
+    //所以我们就要把那个对象“钉”住(pin)，让它的内存地址固定，而不被垃圾回收掉，然后最后我们自己管理，自己释放内存,这时候就需要GCHandle,来看个msdn上的例子:
+    public delegate bool CallBack(int handle, IntPtr param);
+    public class LibWrap
+    {
+        [DllImport("user32.dll")]
+        public static extern bool EnumWindows(CallBack cb, IntPtr param);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TextWriter tw = System.Console.Out;
+            GCHandle gch = GCHandle.Alloc(tw);
+            CallBack cewp = new CallBack(CaptureEnumWindowsProc);
+            LibWrap.EnumWindows(cewp, (IntPtr)gch);
+            gch.Free();
+            Console.Read();
+
+        }
+        private static bool CaptureEnumWindowsProc(int handle, IntPtr param)
+        {
+            GCHandle gch = (GCHandle)param;
+            TextWriter tw = (TextWriter)gch.Target;
+
+            tw.WriteLine(handle);
+            return true;
+        }
+
+    }
+}
--- MultiThread/Program.cs
+++ MultiThread/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,6 +11,11 @@ namespace MultiThread
         static AutoResetEvent AautoResetEvent = new AutoResetEvent(false);
         static AutoResetEvent BautoResetEvent = new AutoResetEvent(false);
         static AutoResetEvent CautoResetEvent = new AutoResetEvent(false);
+
+        //static EventWaitHandle AautoResetEvent = new ManualResetEvent(false);
+        //static EventWaitHandle BautoResetEvent = new ManualResetEvent(false);
+        //static EventWaitHandle CautoResetEvent = new ManualResetEvent(false);
+
         static void ShowA()
         {
             for (int i = 0; i < 10; i++)
@@ -46,4 +53,142 @@ namespace MultiThread
         }
 
     }
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        LoopPrint l = new LoopPrint();
+    //        new Thread(() =>
+    //        {
+    //            for (int i = 1; i <= 20; i++)
+    //            {
+    //                l.loopA(i);
+    //            }
+    //        }).Start();
+    //        new Thread(() =>
+    //        {
+    //            for (int i = 1; i <= 20; i++)
+    //            {
+    //                l.loopB(i);
+    //            }
+    //        }).Start();
+    //        new Thread(() =>
+    //        {
+    //            for (int i = 1; i <= 20; i++)
+    //            {
+    //                l.loopC(i);
+    //            }
+    //        }).Start();
+    //        Console.ReadLine();
+    //    }
+    //}
+    //class LoopPrint
+    //{
+    //    private static int name = 1;
+    //    static object obj = new object();
+    //    public void loopA(int total)
+    //    {
+    //        lock (obj)
+    //        {
+    //            if (name != 1)
+    //            {
+    //                Monitor.Wait(obj);
+    //            }
+    //            for (int i = 0; i <= 5; i++)
+    //            {
+    //                Console.WriteLine("线程id:" + Thread.CurrentThread.ManagedThreadId + "\t A \t " + i + "\t" + total);
+    //            }
+    //            Console.WriteLine("--------loopA结束-----------");
+    //            name = 2;
+    //            Monitor.Pulse(obj);
+    //        }
+    //    }
+    //    public void loopB(int total)
+    //    {
+    //        lock (obj)
+    //        {
+    //            if (name != 2)
+    //            {
+    //                Monitor.Wait(obj);
+    //            }
+    //            for (int i = 0; i <= 10; i++)
+    //            {
+
+    //                Console.WriteLine("线程id:" + Thread.CurrentThread.ManagedThreadId + "\t B \t " + i + "\t" + total);
+    //            }
+    //            name = 3;
+    //            Console.WriteLine("--------loopB结束-----------");
+    //            Monitor.Pulse(obj);
+    //        }
+    //    }
+    //    public void loopC(int total)
+    //    {
+    //        lock (obj)
+    //        {
+    //            if (name != 3)
+    //            {
+    //                Monitor.Wait(obj);
+
+    //            }
+    //            for (int i = 0; i <= 15; i++)
+    //            {
+
+    //                Console.WriteLine("线程id:" + Thread.CurrentThread.ManagedThreadId + "\t C \t " + i + "\t" + total);
+    //            }
+    //            name = 1;
+    //            Console.WriteLine("--------loopC结束-----------");
+    //            Monitor.Pulse(obj);
+    //        }
+    //    }
+
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        //演示前台、后台线程
+    //        BackGroundTest background = new BackGroundTest(10);
+    //        //创建前台线程
+    //        Thread fThread = new Thread(new ThreadStart(background.RunLoop));
+    //        //给线程命名
+    //        fThread.Name = "前台线程";
+
+
+    //        BackGroundTest background1 = new BackGroundTest(20);
+    //        //创建后台线程
+    //        Thread bThread = new Thread(new ThreadStart(background1.RunLoop));
+    //        bThread.Name = "后台线程";
+    //        //设置为后台线程
+    //        bThread.IsBackground = true;
+
+    //        //启动线程
+    //        fThread.Start();
+    //        bThread.Start();
+    //    }
+    //}
+
+    //class BackGroundTest
+    //{
+    //    private int Count;
+    //    public BackGroundTest(int count)
+    //    {
+    //        this.Count = count;
+    //    }
+    //    public void RunLoop()
+    //    {
+    //        //获取当前线程的名称
+    //        string threadName = Thread.CurrentThread.Name;
+    //        for (int i = 0; i < Count; i++)
+    //        {
+    //            Console.WriteLine("{0}计数：{1}", threadName, i.ToString());
+    //            //线程休眠1000毫秒
+    //            Thread.Sleep(1000);
+    //        }
+    //        Console.WriteLine("{0}完成计数", threadName);
+
+    //    }
+    //}
+
+
 }
