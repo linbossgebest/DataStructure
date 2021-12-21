57f35dd98ea1d033f95b1222bf283c957928b655
linbossgebest <809621397@qq.com>
2021-12-21 15:20:14 +0800

    add bubble sort

---
+++ SortingAlgorithm/BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    /// <summary>
+    /// 冒泡排序
+    /// </summary>
+    public class BubbleSort
+    {
+        public int[] SortArray(int[] nums)
+        {
+            if (nums.Length == 0)
+                return nums;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length - 1 - i; j++)
+                {
+                    if (nums[j + 1] > nums[j])
+                    {
+                        var temp = nums[j + 1];
+                        nums[j + 1] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+        
+    }
+}
