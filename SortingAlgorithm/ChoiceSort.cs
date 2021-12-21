1f8d23730f7e9cc98f47799c112e0b235ab91004
linbossgebest <809621397@qq.com>
2021-12-21 15:27:53 +0800

    add ChoiceSort

---
+++ SortingAlgorithm/ChoiceSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    public class ChoiceSort
+    {
+        public int[] SortArray(int[] nums)
+        {
+            if (nums.Length == 0)
+                return nums;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[minIndex])
+                        minIndex = j;
+                }
+                int temp = nums[minIndex];
+                nums[minIndex] = nums[i];
+                nums[i] = temp;
+            }
+            return nums;
+        }
+    }
+}
