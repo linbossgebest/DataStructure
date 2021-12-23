97aed3b7df6c99a62ab9889e2bf3f7073833c1c2
linbossgebest <809621397@qq.com>
2021-12-23 16:44:39 +0800

    add q

---
+++ LeetCode_HOT_100/71Q.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_HOT_100
+{
+
+    //    31. 下一个排列
+    //实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+
+    //如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+
+    //必须 原地 修改，只允许使用额外常数空间。
+
+    //示例 1：
+
+    //输入：nums = [1,2,3]
+    //    输出：[1,3,2]
+    //    示例 2：
+
+    //输入：nums = [3,2,1]
+    //    输出：[1,2,3]
+    //    示例 3：
+
+    //输入：nums = [1,1,5]
+    //    输出：[1,5,1]
+    //    示例 4：
+
+    //输入：nums = [1]
+    //    输出：[1]
+
+
+    //    提示：
+
+    //1 <= nums.length <= 100
+    //0 <= nums[i] <= 100
+    public class Solution71
+    {
+        public void NextPermutation(int[] nums)
+        {
+            int i = nums.Length - 2;
+            while (i >= 0 && nums[i] >= nums[i + 1])
+                i--;
+            if (i >= 0)
+            {
+                int j = nums.Length - 1;
+                while (j >= 0 && nums[i] >= nums[j])
+                    j--;
+
+                Swap(nums, i, j);
+            }
+            Reverse(nums, i + 1);
+        }
+
+        public void Swap(int[] nums, int i, int j)
+        {
+            var temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+        }
+
+        public void Reverse(int[] nums, int start)
+        {
+            int left = start;
+            int right = nums.Length - 1;
+            while (left < right)
+            {
+                Swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+    }
+}
